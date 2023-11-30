0153da88d552ec8caa9628a4b3b4dc85443ccfb1
expenseTracker2023 <151793780+expenseTracker2023@users.noreply.github.com>
2023-11-30 13:24:41 -0800

    Update passport.ts

--- server/passport/passport.ts
+++ server/passport/passport.ts
@@ -8,40 +8,40 @@ dotenv.config();
 
 const GoogleStrategy = passportGoogle.Strategy;
 
-// passport.serializeUser((user: any, done) => {
-//   done(null, user.id);
-// });
+passport.serializeUser((user: any, done) => {
+  done(null, user.id);
+});
 
-// passport.deserializeUser(async (id, done) => {
-//   const user = await User.findById(id);
-//   done(null, user);
-// });
+passport.deserializeUser(async (id, done) => {
+  const user = await User.findById(id);
+  done(null, user);
+});
 
-const googleStrategy = new GoogleStrategy(
-  {
-    clientID: process.env.GOOGLE_CLIENT_ID as string,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    callbackURL: `${process.env.SERVER}/auth/redirect/google`,
-  },
-  async (accessToken, refreshToken, profile, done) => {
-    // get profile details
-    // save profile details in db
-    const user = await User.findOne({ googleId: profile.id });
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      callbackURL: '/auth/redirect/google',
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      // get profile details
+      // save profile details in db
+      const user = await User.findOne({ googleId: profile.id });
 
-    if (!user) {
-      const newUser = await User.create({
-        googleId: profile.id,
-        name: profile.displayName,
-        email: profile.emails?.[0].value,
-        picture: profile.photos?.[0].value,
-      });
-      if (newUser) {
-        done(null, newUser);
+      if (!user) {
+        const newUser = await User.create({
+          googleId: profile.id,
+          name: profile.displayName,
+          email: profile.emails?.[0].value,
+          picture: profile.photos?.[0].value,
+        });
+        if (newUser) {
+          done(null, newUser);
+        }
+      } else {
+        done(null, user);
       }
-    } else {
-      done(null, user);
     }
-  }
+  )
 );
-
-export { googleStrategy };
